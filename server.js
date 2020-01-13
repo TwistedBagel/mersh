d39cc27bcc3b1419f2f2ace065e8bdd7396a50fe
TwistedBagel <darthmars97@gmail.com>
2020-01-13 11:15:49 -0700

    add morgan for debug

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
     res.render('pages/index');
